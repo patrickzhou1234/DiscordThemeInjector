c3fa48eabffea2b831001cdf3d018e935f673527
Patrick <83720283+patrickzhou1234@users.noreply.github.com>
2022-10-21 17:19:02 -0700

    Create SoftX.js

---
+++ SoftX.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         SoftX
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://discord.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=stackoverflow.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var link = document.createElement("link");
+link.href = "https://cdn.discordapp.com/attachments/965460197072044062/1033172224892489818/SoftX.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+document.getElementsByTagName("head")[0].appendChild(link);
+})();
