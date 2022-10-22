9f4f9740a3bee31b9fa85f954872cc357de09b5d
Patrick <83720283+patrickzhou1234@users.noreply.github.com>
2022-10-21 17:58:14 -0700

    Create DarkMatter.js

---
+++ DarkMatter.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         DarkMatter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://discord.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=stackoverflow.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var link = document.createElement("link");
+link.href = "https://cdn.discordapp.com/attachments/965460197072044062/1033182017589887006/DarkMatter.theme.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+document.getElementsByTagName("head")[0].appendChild(link);
+})();
