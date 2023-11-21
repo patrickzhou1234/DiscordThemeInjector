8b7b5c1ab69e03f3512423bc3a5173893b5d10d1
Patrick Zhou <patrickchang44543@gmail.com>
2023-11-21 12:13:54 -0800

    Update SoftX.js

--- SoftX.js
+++ SoftX.js
@@ -12,7 +12,7 @@
 (function() {
     'use strict';
     var link = document.createElement("link");
-link.href = "https://cdn.discordapp.com/attachments/965460197072044062/1138697460009680968/SoftX.theme.css";
+link.href = "https://cdn.discordapp.com/attachments/965460197072044062/1176616113992368250/SoftX.theme.css";
 link.type = "text/css";
 link.rel = "stylesheet";
 document.getElementsByTagName("head")[0].appendChild(link);
